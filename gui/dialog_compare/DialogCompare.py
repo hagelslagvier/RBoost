768cabc997202b2334890a8f5e644d6cefc92b8d
Alexey Naumov <rocketbuzzz@gmail.com>
2019-01-18 21:01:31 +0300

    Add 'Next' shortcut.

--- gui/dialog_compare/DialogCompare.py
+++ gui/dialog_compare/DialogCompare.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from PyQt5.QtCore import Qt, QEvent
 from PyQt5.QtWidgets import QDialog
 from gui.dialog_compare.Ui_DialogCompare import Ui_DialogCompare
 
@@ -10,7 +10,22 @@ class DialogCompare(Ui_DialogCompare, QDialog):
         QDialog.__init__(self, parent)
 
         self.setupUi(self)
+
+        self.__customize()
+
+    def __customize(self):
+        self.textEditUserAnswer.installEventFilter(self)
+        self.textEditCorrectAnswer.installEventFilter(self)
+
         self.pushButtonOk.clicked.connect(self.onPushButtonOkClicked)
+
+    def eventFilter(self, receiver, event):
+        if QEvent.KeyPress == event.type() and event.key() == Qt.Key_Return:
+            self.pushButtonOk.click()
+
+            return True
+
+        return QDialog.eventFilter(self, receiver, event)
 
     def onPushButtonOkClicked(self):
         self.close()
