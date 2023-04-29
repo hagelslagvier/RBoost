73c4349e41f0d16ef00fe1cac12f87c68d520628
Alexey Naumov <alexey.o.naumov@gmail.com>
2023-04-29 16:26:59 +0200

    remove unused attributes

--- gui/dialog_item_edit/DialogItemEdit.py
+++ gui/dialog_item_edit/DialogItemEdit.py
@@ -4,17 +4,12 @@ from gui.base.dialog_item.BaseDialogItem import BaseDialogItem
 class DialogItemEdit(BaseDialogItem):
     def __init__(self, parent=None):
         BaseDialogItem.__init__(self, parent)
-        self.expressionToChange = None
 
         self.__customize()
 
     def __customize(self):
         self.setWindowTitle("Изменить")
         self.pushButtonOk.setText("Изменить")
-
-    def setExpression(self, text):
-        self.expressionToChange = text
-        BaseDialogItem.setExpression(self, text)
 
     def hideEvent(self, event):
         self.textEditExpression.clear()
